3406006681f22e28bfd76c1a3b61af25c54a0abf
iijawhor <jawhorali1@gmail.com>
2025-01-25 23:14:23 +0530

    feat(logo): created a  logo with dot container - Added dot container with animated dots for logo design - Displayed 'sazi' as the logo title

---
+++ src/componenets/Logo/Logo.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Logo.css";
+const Logo = () => {
+  return (
+    <>
+      <div className="logo">
+        <div className="dot-container">
+          <span className="dot dot-one"></span>
+          <span className="dot dot-two"></span>
+          <span className="dot dot-three"></span>
+        </div>
+        <span className="logo-title">sazi</span>
+      </div>
+    </>
+  );
+};
+
+export default Logo;
