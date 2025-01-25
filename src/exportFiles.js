a076a704bea047c5afeff3376a805d888fde4f75
iijawhor <jawhorali1@gmail.com>
2025-01-25 23:05:24 +0530

    added new files to export

--- src/exportFiles.js
+++ src/exportFiles.js
@@ -4,6 +4,31 @@ import Input from "./componenets/Input/Input";
 import Button from "./componenets/Button/Button";
 import Register from "./auth/Register/Register";
 import Login from "./auth/Login/Login";
-import Layout from "./componenets/Layout/Layout";
-
-export { Register, Input, Button, getData, postData, Login, Layout };
+import Sidebar from "./componenets/Sidebar/Sidebar";
+import Home from "./pages/Home/Home";
+import Logo from "./componenets/Logo/Logo";
+import AssignmentPage from "./pages/assignment/AssignmentPage";
+import Header from "./componenets/Header/Header";
+import Logout from "./auth/Logout/Logout";
+import CreateAssignment from "./componenets/CreateAssignment/CreateAssignment";
+import Assignment from "./componenets/Assignment/Assignment";
+import truncateText from "./utility/truncateText";
+import AssignmentSubmission from "./componenets/AssignmentSubmission/AssignmentSubmission";
+export {
+  Register,
+  Input,
+  Button,
+  getData,
+  postData,
+  Login,
+  Sidebar,
+  Home,
+  Logo,
+  AssignmentPage,
+  Header,
+  Logout,
+  CreateAssignment,
+  Assignment,
+  truncateText,
+  AssignmentSubmission
+};
