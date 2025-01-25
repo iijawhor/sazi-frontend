a1d51e651cc1d45d63b7e573b3a2f2a145db8035
iijawhor <jawhorali1@gmail.com>
2025-01-25 23:03:05 +0530

    Refactor Redux setup by adding relevant reducers for assignment, header, and user management, and cleaning up the uthSlice to match the correct structure.

--- src/store/slices/authSlice.js
+++ src/store/slices/authSlice.js
@@ -1,23 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {};
+const initialState = {
+  userDetails: {},
+  logout: ""
+};
 
 export const authSlice = createSlice({
-  name: "counter",
+  name: "user",
   initialState,
   reducers: {
-    createUser: (state) => {
-      state.value += 1;
+    createUser: (state) => {},
+    loginUser: (state) => {},
+    updateUser: (state, action) => {},
+    userDetails: (state, action) => {
+      state.userDetails = action.payload;
     },
-    loginUser: (state) => {
-      state.value -= 1;
-    },
-    updateUser: (state, action) => {
-      state.value += action.payload;
-    }
+    userLogout: (state, action) => {}
   }
 });
 
-export const { createUser, loginUser, updateUser } = counterSlice.actions;
+export const { createUser, loginUser, updateUser, userDetails } =
+  authSlice.actions;
 
 export default authSlice.reducer;
--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import assignmentReducer from "./slices/assignmentSlice";
+import headerReducer from "./slices/updateHeaderSlice";
+import userReducer from "./slices/authSlice";
 export const store = configureStore({
-  reducer: {}
+  reducer: {
+    assignment: assignmentReducer,
+    header: headerReducer,
+    user: userReducer
+  }
 });
