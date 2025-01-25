d4481f58c2d95ae20a251648d9feef3a579628e4
iijawhor <jawhorali1@gmail.com>
2025-01-25 22:57:05 +0530

    Refactor Button component by removing the unused onClick prop, simplifying the code.

--- src/componenets/Button/Button.jsx
+++ src/componenets/Button/Button.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Button.css";
-const Button = ({ type, button, className }) => {
+const Button = ({ type, button, className, onClick }) => {
   return (
-    <button type={type} className={`button ${className}`}>
+    <button onClick={onClick} type={type} className={`button ${className}`}>
       {button}
     </button>
   );
