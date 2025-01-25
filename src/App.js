34abbdc41452379400d9639fdab42c8f1420581a
iijawhor <jawhorali1@gmail.com>
2025-01-25 23:00:53 +0530

    Refactor App component by removing unused imports (Header, Sidebar, Register) and simplifying the code by displaying user details directly after login.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import { useState } from "react";
 import "./App.css";
-import { Login, Register } from "./exportFiles";
+import { Header, Login, Register, Sidebar } from "./exportFiles";
+import { Outlet } from "react-router";
 function App() {
+  const sidebar = [
+    { name: "home", path: "", icon: "", active: "exact" },
+    { name: "my class", path: "my-class", icon: "" },
+    { name: "assignments", path: "assignments", icon: "" },
+    { name: "attendance", path: "attendance", icon: "" },
+    { name: "grade book", path: "grade-book", icon: "" },
+    { name: "calender", path: "calender", icon: "" },
+    { name: "notification", path: "notification", icon: "" },
+    { name: "profile", path: "profile", icon: "" }
+  ];
   const [userDetails, setUserDetails] = useState(null);
-  console.log("userDetails........", userDetails);
 
   if (!userDetails) {
     return (
@@ -14,9 +24,17 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <h1>Saji for Teacher</h1>
-      {userDetails.user.fullname}
+    <div className="app">
+      <div className="app-sidebar-container">
+        <Sidebar sidebar={sidebar} />
+      </div>
+      <div className="app-outlet-container">
+        <div className="app-header-container">
+          <Header />
+        </div>
+        <Outlet />
+      </div>
+      <div className="app-widgets">widgets section</div>
     </div>
   );
 }
